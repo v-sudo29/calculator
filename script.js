ae445405c982f33dda860ba171e1196db179403a
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-02-19 17:40:26 -0800

    Create operate function

--- script.js
+++ script.js
@@ -21,3 +21,17 @@ function divide(...num) {
     let quotient = num.reduce((a, b) => a / b);
     return quotient; 
 }
+
+// Operate function
+function operate(operator, a, b) {
+    if (operator === '+') {
+        return sum(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    } 
+    return;
+}
