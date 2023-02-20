499687ee9330f641007446843bb93cf100cce738
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-02-19 17:32:13 -0800

    Create divide function

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 // Add function
 function sum(...num) {
-     let sum = num.reduce((a, b) => a + b, 0);
+     let sum = num.reduce((a, b) => a + b);
     return sum;
 }
 
 // Subtract function
 function subtract(...num) {
-    let sum = num.reduce((a, b) => {
-        return a - b;
-    });
+    let sum = num.reduce((a, b) => a - b);
     return sum;
 }
 
 // Multiply function
 function multiply(...num) {
-    let product = num.reduce((a, b) => {
-        return a * b;
-    });
+    let product = num.reduce((a, b) => a * b);
     return product;
 }
 
+// Divide function
+function divide(...num) {
+    let quotient = num.reduce((a, b) => a / b);
+    return quotient; 
+}
